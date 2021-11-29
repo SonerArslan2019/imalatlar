7a09b29bfcd3b963274a1bb9a041a14b5b5e7977
cprm <https://github.com/SonerArslan2019/imalatlar.git>
2021-11-29 18:10:46 +0300

    template devami

--- imalatlar/apps/sld/views.py
+++ imalatlar/apps/sld/views.py
@@ -20,7 +20,8 @@ def detail_view(request, id):
 @login_required
 def create_view(request):
     if request.method == 'POST':
-        form = SLDForm(request.POST)  # request.post un icinden gelen veriler ile bana bir form hazirla, onuda form a atadik
+        form = SLDForm(
+            request.POST)  # request.post un icinden gelen veriler ile bana bir form hazirla, onuda form a atadik
 
         if form.is_valid():  # kayit valid ise asagidaki islemlere gecilir degilse, else blogu calisir
             instance = form.save(commit=False)
@@ -44,10 +45,7 @@ def list_view(request):
 
 
 @login_required
-def send_mail(request, id):
-    # tmp dizininin Windowsta olmamasından dolayı windowsta çalışmayabilir
-    # wkhtmktopdf'i sunucuya kurmayı unutmayın!
-    # Localde çalışırken default 8000 portunda çalıştırın!
+def make_pdf(request, id):
     door = get_object_or_404(SLD, id=id)
 
     curr_url = ALLOWED_HOSTS[0].replace('http://', '').replace('https://', '') if ALLOWED_HOSTS else '127.0.0.1:8000'
@@ -66,9 +64,35 @@ def send_mail(request, id):
         f'/tmp/{door.crm}.html',
         pdf_file
     ])
-    send_html_mail(
-        f'{door.crm} - {door.get_door_type_display()}',
-        '<p>Kapının teknik detayları ekteki pdftedir.</p>',
-        file=pdf_file
-    )
-    return HttpResponse('Mail Başarıyla gönderildi.')
+
+
+
+# @login_required
+# def send_mail(request, id):
+#     # tmp dizininin Windowsta olmamasından dolayı windowsta çalışmayabilir
+#     # wkhtmktopdf'i sunucuya kurmayı unutmayın!
+#     # Localde çalışırken default 8000 portunda çalıştırın!
+#     door = get_object_or_404(SLD, id=id)
+#
+#     curr_url = ALLOWED_HOSTS[0].replace('http://', '').replace('https://', '') if ALLOWED_HOSTS else '127.0.0.1:8000'
+#     html = render_to_string('sld/detail.html', {'work_order': door, 'print': True}).replace('/static/',
+#                                                                                             f'http://{curr_url}/static/')
+#
+#     with open(f'/tmp/{door.crm}.html', 'w') as f:
+#         f.write(html)
+#
+#     pdf_file = f'/tmp/sld_{door.door_type}_{door.id}.pdf'
+#     subprocess.run([
+#         'wkhtmltopdf',
+#         '--javascript-delay', '2000',
+#         '--viewport-size', '1024x768',
+#         '--load-error-handling', 'ignore',
+#         f'/tmp/{door.crm}.html',
+#         pdf_file
+#     ])
+#     send_html_mail(
+#         f'{door.crm} - {door.get_door_type_display()}',
+#         '<p>Kapının teknik detayları ekteki pdftedir.</p>',
+#         file=pdf_file
+#     )
+#     return HttpResponse('Mail Başarıyla gönderildi.')
